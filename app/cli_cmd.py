c241adbd9b1a8ed391ddefddf8541eb154820c65
MoustacheGolem <mateus_p_r@hotmail.com>
2023-07-06 16:30:12 -0300

    fix login com email

--- app/cli_cmd.py
+++ app/cli_cmd.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
-from app import db
+from app import db, bcrypt
+
 from .models.tables import QuestaoCE, User
 from .seed import questionCE, users
 seed_cli = AppGroup("seed")
@@ -16,7 +17,9 @@ def seed_questionCE():
 @seed_cli.command("user")
 def seed_user():
     "Add seed data to the database."
+    
     for user in users:
+        user['password'] = bcrypt.generate_password_hash(user.get('password'))
         db.session.add(User(**user))
     db.session.commit()
 
--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -29,8 +29,8 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
-            #if bcrypt.check_password_hash(user.password, form.password.data):
-            if user.password == form.password.data:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+            # if user.password == form.password.data:
                 login_user(user)
                 return redirect(url_for('user'))
     return render_template('login.html', form=form)
@@ -41,7 +41,9 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
+        
+        new_user = User(username=form.username.data, email = form.email.data, password=hashed_password)
+       
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
